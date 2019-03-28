34583d6aa635abf3e44c9bb48d10a291bbdd5790
Hannah Moncot <hannah.moncot@gmail.com>
2019-03-28 15:14:10 +0100

    Second quest React My Travels

--- src/App.js
+++ src/App.js
@@ -1,4 +1,33 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+// import './App.css';
+// import Travel from './Travel';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <Travel 
+//           destination='Chichiriviche '
+//           country='Venezuela '
+//           distance='7287 kms'
+//           photo='https://upload.wikimedia.org/wikipedia/commons/5/5c/20160630_175251_Amanecer_desde_Chichiriviche.png'
+//         />
+//         <Travel
+//           destination='London '
+//           country='United Kingdom '
+//           distance='773 kms'
+//           photo='https://cdn.pixabay.com/photo/2016/06/07/11/02/london-1441427_960_720.png'
+//         />
+//       </div>
+        
+//     );
+//   }
+// }
+
+// ******************* fin de quête React 1 ************************
+
+
+import React, {Component} from "react";
 import './App.css';
 import Travel from './Travel';
 
@@ -6,20 +35,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Travel 
-          destination='Chichiriviche '
-          country='Venezuela '
-          distance='7287 kms'
-          photo='https://upload.wikimedia.org/wikipedia/commons/5/5c/20160630_175251_Amanecer_desde_Chichiriviche.png'
-        />
-        <Travel
-          destination='London '
-          country='United Kingdom '
-          distance='773 kms'
-          photo='https://cdn.pixabay.com/photo/2016/06/07/11/02/london-1441427_960_720.png'
-        />
+        <Travel />
       </div>
-        
     );
   }
 }
--- src/Travel.js
+++ src/Travel.js
@@ -1,23 +1,82 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class Travel extends Component {
-    constructor(props){
-        super(props)
-    }
-    render() {
-        return (
-            <div>
-                <h1>
-                {this.props.destination} 
-                {this.props.country} 
-                </h1>
-                <p>Cette destination est à une distance de {this.props.distance} de Saint Jean de Luz</p>
-                <img src={this.props.photo} />
+// class Travel extends Component {
+//     constructor(props){
+//         super(props)
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <h1>
+//                 {this.props.destination} 
+//                 {this.props.country} 
+//                 </h1>
+//                 <p>Cette destination est à une distance de {this.props.distance} de Saint Jean de Luz</p>
+//                 <img src={this.props.photo} />
                 
-            </div>
-        );
-    }
-};
+//             </div>
+//         );
+//     }
+// };
 
+
+// export default Travel;
+
+// ******************** quête React 1 ************************
+
+// src/Travel.js
+import React, {Component} from 'react';
+
+const travels = [
+    {
+        "id": "1",
+        "destination": "Chichiriviche",
+        "country": "Venezuela",
+        "distance": "7287 kms",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/5/5c/20160630_175251_Amanecer_desde_Chichiriviche.png"
+    },
+    {
+        "id": "2",
+        "destination": "London",
+        "country": "United Kingdom",
+        "distance": "776 km",
+        "photo": "https://cdn.pixabay.com/photo/2016/06/07/11/02/london-1441427_960_720.png"
+    },
+    {
+        "id": "3",
+        "destination": "Malaga",
+        "country": "Spain",
+        "distance": "800 km",
+        "photo": "https://blog.fuertehoteles.com/wp-content/uploads/2017/04/malaga-sunset.jpg"
+    },
+    {
+        "id": "4",
+        "destination": "Ho Chi Min",
+        "country": "Vietnam",
+        "distance": "4200 km",
+        "photo": "https://www.fr.lastminute.com/vol/lastminute/img/ho_chi_minh_ville.jpg"
+    },
+    {
+        "id": "5",
+        "destination": "New York",
+        "country": "U.S.A",
+        "distance": "2100 km",
+        "photo": "https://images.musement.com/cover/0002/49/thumb_148242_cover_header.jpeg?&q=60&fit=crop&h=400&lossless=true&auto=format&w=1398"
+    }
+]
+
+const Travel = (props) => (
+    <div class="travel">
+        <p>
+            {travels.map(travel => (
+                <div>
+                    <p>Destination: {travel.destination}, country: {travel.country}, distance: {travel.distance}</p>
+                    <img src={travel.photo} alt="building" />
+                </div>
+            ))}
+        </p>
+        <hr />
+    </div>
+);
 
 export default Travel;
